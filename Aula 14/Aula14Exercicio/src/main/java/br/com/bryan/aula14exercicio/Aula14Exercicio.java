c72b8718d1515ea07c183ba2e948b5b288474cec
Bryan Gabriel <gabrielbryan717@gmail.com>
2025-12-26 02:07:19 -0300

    Adicionando mais exercicios na Pasta Aula 14 Aula14Exercicio

--- Aula 14/Aula14Exercicio/src/main/java/br/com/bryan/aula14exercicio/Aula14Exercicio.java
+++ Aula 14/Aula14Exercicio/src/main/java/br/com/bryan/aula14exercicio/Aula14Exercicio.java
@@ -50,7 +50,7 @@ public class Aula14Exercicio {
             System.out.print(num[j] + " ");
         }*/
         
-        Scanner ler = new Scanner(System.in);
+        /*Scanner ler = new Scanner(System.in);
         
         int num[] = new int [6];
         
@@ -76,6 +76,67 @@ public class Aula14Exercicio {
             }
         }
         System.out.println("Maior valor: " + maiorNum);
-        System.out.println("Menor valor: " + menorNum);
+        System.out.println("Menor valor: " + menorNum);*/
+        
+        /*int n[] = {8,55,7,4,10,9,45,3,7,4};
+        
+        int nPar=0;
+        int nImpar=0;
+        
+        for (int i: n){
+            if(i % 2 ==0){
+                System.out.print(i + " ");
+                nPar++;
+            } else{
+                System.out.print(i + " ");
+                nImpar++;
+            }
+        }
+        System.out.println("");
+        System.out.println("Existem numeros Pares: " + nPar);
+        System.out.println("Existem numeros Impares: " + nImpar);*/
+        
+        Scanner ler = new Scanner(System.in);
+        
+        String aluno[] = new String[5];
+        
+        for(int i =0; i < aluno.length; i++){
+            System.out.print("Digite o nome do "+ (i + 1) +"º aluno: ");
+            aluno[i] = ler.nextLine();
+        }
+        System.out.println("");
+        
+        float nota[] = new float[5];
+        float media = 0;
+        float soma = 0;
+        
+        for (int i =0; i < nota.length; i++){
+            System.out.print("Digite a nota do aluno(a) " + aluno[i] + ": ");
+            nota[i] = ler.nextFloat();
+            soma += nota[i];
+        }
+        System.out.println("");
+        
+        media = (float) soma / nota.length;
+        if (media > 0){
+            System.out.printf("Media dos alunos: %.2f", media);
+            System.out.println("");
+            System.out.println("ACIMA DA MEDIA");
+            for (int i = 0; i < nota.length; i++){
+                if (nota[i] >= media){
+                    System.out.println(aluno[i] + ": " + nota[i]);
+                }
+            }
+        } 
+        System.out.println("");
+        
+        if (media > 0){
+            System.out.println("ABAIXO DA MEDIA");
+            for (int i = 0; i < nota.length; i++){
+                if (nota[i] < media){
+                    System.out.println(aluno[i] + ": " + nota[i]);
+                }
+            }
+        }
     }
 }
